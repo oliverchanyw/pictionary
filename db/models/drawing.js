a1986ff6b6532af9a7f80fc12fe3a6203421bacc
oliverchanyw <oliverchanyw@gmail.com>
2017-11-29 09:17:04 -0500

    More schemas for mongoose

---
+++ db/models/drawing.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var PixelSchema = require('./pixel').schema;
+
+var drawingSchema = mongoose.Schema({
+  Labels: {
+    type: [String],
+    required: true,
+  },
+  Image: [PixelSchema]
+});
+
+module.exports = mongoose.model('Drawing', drawingSchema);
